17cbed1df6a75aeb44f6fa8ac347a2c7d9c35a63
darshan <darsh2605@gmail.com>
2024-01-07 13:42:34 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/darshankomu/days_flashcards_for_kids/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.darshankomu.days_flashcards_for_kids
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+const kprimarycolor = Color(0xff1C3249);
+const ksecondcolor = Color(0xff3ba2ff);
+const kthirdcolor = Color(0xff006ac0);
+const ksplashback = Color(0xffD48E7E);
+const kback = Color(0xffD48E7E);
+const backcolor = Color(0xFFD48E7E);
+const backcolor_2 = Color(0xFFFFFFFF);
+
+
+
+var androidAppShareLink = "https://play.google.com/store/apps/details?id=com.darshankomu.days_flashcards_for_kids";
+
+var androidShareText = "Download Days Planets for Kids app from below link";
+
+var androidAppIdValue = "com.darshankomu.days_flashcards_for_kids";
+
+var iOSAppIdValue = "AddiOSAppId";
